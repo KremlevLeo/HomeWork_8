84737379e4736a0a4da7dede58e37f6e99230b21
Леонид Кремлев <62460369+KremlevLeo@users.noreply.github.com>
2022-08-23 21:24:58 +0500

    Task002 not yet

---
+++ Task_001/Program.cs
@@ -0,0 +1,70 @@
+﻿/*Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+1 2 4 7
+2 3 5 9
+2 4 4 8*/
+
+Console.Write("Enter number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter number of colums: ");
+int col = int.Parse(Console.ReadLine());
+int[,] arr = FillArray(row, col);
+Console.WriteLine("Sort array");
+PrintArray(SortArrStr(arr));
+
+
+void PrintArray(int [,] arr)
+{
+
+    for (int r = 0; r < arr.GetLength(0); r++)
+    {
+        for (int c = 0; c < arr.GetLength(1); c++)
+        {
+            Console.Write($"{arr[r, c]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] SortArrStr(int[,] arr)
+{
+
+    for (int r = 0; r < arr.GetLength(0); r++)
+    {
+        int temp;
+        for (int c = 0; c < arr.GetLength(1); c++)
+        {
+            for (int cx = c + 1; cx < arr.GetLength(1); cx++)
+            {
+                if (arr[r, c] < arr[r, cx])
+                {
+                    temp = arr[r, c];
+                    arr[r, c] = arr[r, cx];
+                    arr[r, cx] = temp;
+                }
+            }
+        }
+    }
+    return arr;
+}
+
+int[,] FillArray(int row, int col)
+{
+    int[,] arr = new int[row, col];
+    Random rnd = new Random();
+    for (int r = 0; r < arr.GetLength(0); r++)
+    {
+        for (int c = 0; c < arr.GetLength(1); c++)
+        {
+            arr[r, c] = rnd.Next(1, 10);
+            Console.Write($"{arr[r, c]} \t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    return arr;
+}
---
+++ Task_002/Program.cs
@@ -0,0 +1,63 @@
+﻿/*Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка*/
+
+Console.Write("Enter number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter number of colums: ");
+int col = int.Parse(Console.ReadLine());
+int[,] arr = FillArray(row, col);
+int res = FaindRow(SumOfRow(arr));
+Console.WriteLine(res);
+
+
+int[,] FillArray(int row, int col)
+{
+    int[,] arr = new int[row, col];
+    Random rnd = new Random();
+    for (int r = 0; r < arr.GetLength(0); r++)
+    {
+        for (int c = 0; c < arr.GetLength(1); c++)
+        {
+            arr[r, c] = rnd.Next(1, 10);
+            Console.Write($"{arr[r, c]} \t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    return arr;
+}
+
+int[] SumOfRow(int[,] arr)
+{
+    int[] sum = new int[arr.Length];
+    int tmp;
+    for (int r = 0; r < arr.GetLength(0); r++)
+    {
+        tmp = 0;
+        for (int c = 0; c < arr.GetLength(1); c++)
+        {
+            tmp += arr[r, c];
+        }
+        sum[r] = tmp;
+    }
+    return sum;
+}
+int FaindRow(int[] arr)
+{
+    for (int i = 1; i < arr.Length; i++)
+    {
+        int min = arr[0];
+        if (min > arr[i])
+        {
+            min = arr[i];
+        }
+        int index = Array.IndexOf(arr, min);
+        return index;
+    }
+    return -1;
+}
